3bdbc951641967b19718f181628f8fee966e5f0d
BisaAja <sabrina@ti.ukdw.ac.id>
2026-08-06 14:42:12 +0700

    fix connection kpncorp

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles, LogsActivity;
 
+    protected $connection = 'kpncorp';
+    protected $table = 'users';
+
     /** Ladder job level: angka besar = jabatan lebih tinggi (T-1/12/71/86). */
     public const JOB_LEVELS = [
         1 => 'Staff',
