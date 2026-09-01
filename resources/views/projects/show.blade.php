79277adc9723397dbafe55be4fa7e61556ba6b21
BisaAja <sabrina@ti.ukdw.ac.id>
2026-09-01 14:23:10 +0700

    Revise Terms in Project Proposal

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -400,8 +400,8 @@
                                                     <x-record-version :model="$plan" />
                                                 <div><label class="block text-xs font-semibold text-gray-600 mb-1">Activity</label><input name="activity" value="{{ $plan->activity }}" required class="{{ $inp }} w-full"></div>
                                                 <div class="grid grid-cols-2 gap-2">
-                                                    <label class="text-xs text-gray-500">Planning Start<input type="date" name="planning_start" value="{{ optional($plan->planning_start)->format('Y-m-d') }}" class="{{ $inp }} w-full"></label>
-                                                    <label class="text-xs text-gray-500">Planning End<input type="date" name="planning_end" value="{{ optional($plan->planning_end)->format('Y-m-d') }}" class="{{ $inp }} w-full"></label>
+                                                    <label class="text-xs text-gray-500">Planned Start Date<input type="date" name="planning_start" value="{{ optional($plan->planning_start)->format('Y-m-d') }}" class="{{ $inp }} w-full"></label>
+                                                    <label class="text-xs text-gray-500">Planned End Date<input type="date" name="planning_end" value="{{ optional($plan->planning_end)->format('Y-m-d') }}" class="{{ $inp }} w-full"></label>
                                                 </div>
                                                 <div><label class="block text-xs font-semibold text-gray-600 mb-1">PIC</label>
                                                     <select name="pic_user_ids[]" multiple placeholder="Click to select PIC (you can choose more than one)…" class="{{ $inp }} w-full">
@@ -444,8 +444,8 @@
                                 <div class="border rounded-lg p-3 grid grid-cols-2 gap-2 relative">
                                     <button type="button" x-show="rows.length > 1" @click="rows.splice(i, 1)" class="absolute top-1.5 right-2 text-red-600 text-sm">&times;</button>
                                     <div class="col-span-2"><label class="block text-xs font-semibold text-gray-600 mb-1">Activity</label><input :name="'rows['+i+'][activity]'" x-model="row.activity" placeholder="Activity" required class="{{ $inp }} w-full"></div>
-                                    <label class="text-xs text-gray-500">Planning Start<input type="date" :name="'rows['+i+'][planning_start]'" x-model="row.planning_start" class="{{ $inp }} w-full"></label>
-                                    <label class="text-xs text-gray-500">Planning End<input type="date" :name="'rows['+i+'][planning_end]'" x-model="row.planning_end" class="{{ $inp }} w-full"></label>
+                                    <label class="text-xs text-gray-500">Planned Start Date<input type="date" :name="'rows['+i+'][planning_start]'" x-model="row.planning_start" class="{{ $inp }} w-full"></label>
+                                    <label class="text-xs text-gray-500">Planned End Date<input type="date" :name="'rows['+i+'][planning_end]'" x-model="row.planning_end" class="{{ $inp }} w-full"></label>
                                     @if($showActual)
                                     <label class="text-xs text-gray-500">Actual Start<input type="date" :name="'rows['+i+'][actual_start]'" x-model="row.actual_start" class="{{ $inp }} w-full"></label>
                                     <label class="text-xs text-gray-500">Actual End<input type="date" :name="'rows['+i+'][actual_end]'" x-model="row.actual_end" class="{{ $inp }} w-full"></label>
